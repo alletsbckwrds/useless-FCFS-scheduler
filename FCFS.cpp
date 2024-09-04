3260291225f4b9be02a46b10ca58fccb04205103
allets <48606593+alletsbckwrds@users.noreply.github.com>
2024-09-04 15:15:26 +0200

    now it's purely c.
    deleted all the c++ bits, and rewrote them in c

--- FCFS.cpp
+++ FCFS.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
+//#include <iostream>
 #include <stdlib.h>
+#include <stdio.h>
 
 struct processes
 {
@@ -13,20 +14,20 @@ void FCFScalc(struct processes *array_progs, int num_progs); //prototipo di funz
 
 int main()
 {
-	std::cout<<"Quanti programmi vuoi inserire? ";
+	printf("Quanti programmi vuoi inserire? ");
 	int num_progs;
-	std::cin>>num_progs;
+	scanf("%d", &num_progs);
 	
 	while(num_progs < 2)
 	{
-		std::cout<<"Devi inserire un numero maggiore di 1.\n";
-		std::cin>>num_progs;
+		printf("Devi inserire un numero maggiore di 1.\n");
+		scanf("%d", &num_progs);
 	}
 	
 	struct processes *array_progs = malloc(num_progs * sizeof(*array_progs)); //array di lunghezza arbitraria di struct per ogni processo
 	if(array_progs == NULL)
 	{
-		std::cout<<"Impossibile allocare risorse per l'esecuzione del programma.\n";
+		printf("Impossibile allocare risorse per l'esecuzione del programma.\n");
 		return 1;
 	}
 	
@@ -40,15 +41,15 @@ int main()
 
 void input(struct processes *array_progs, int num_progs)
 {
-	std::cout<<"L'arrival time deve partire da 0.\n\n";
+	printf("L\'arrival time deve partire da 0.\n\n");
 	for(int i=0; i<num_progs; i++) //ciclo che si ripete tante volte quante le struct nell'array
 	{
 		array_progs[i].num = i;					//setta il numero del processo
-		std::cout<<"Burst time per processo P"<<i<<": ";
-		std::cin>>array_progs[i].burst_time;	//input per burst time
+		printf("Burst time per processo P%d: ", i);
+		scanf("%d", &(array_progs[i].burst_time)); //std::cin>>array_progs[i].burst_time;	//input per burst time
 		
-		std::cout<<"Arrival time per processo P"<<i<<": ";
-		std::cin>>array_progs[i].arrival_time;	//input per arrival time
+		printf("Arrival time per processo P%d: ", i);
+		scanf("%d", &(array_progs[i].arrival_time)); //std::cin>>array_progs[i].arrival_time;	//input per arrival time
 	}
 }
 
@@ -63,58 +64,59 @@ void FCFScalc(struct processes *array_progs, int num_progs)
 				array_progs[j+1] = temp;				//
 			}								//
 
-	std::cout<<"Processi:\t\t";			//
+	printf("Processi:\t\t");			//
 	for(int i=0; i<num_progs; i++)			//
-		std::cout<<array_progs[i].num<<"\t";	//output numero processi
-	std::cout<<"\n";				//
+		printf("%d\t", array_progs[i].num);	//output numero processi
+	printf("\n");				//
 
-	std::cout<<"Burst time:\t\t";				//
+	printf("Burst time:\t\t");				//
 	for(int i=0; i<num_progs; i++)				//
-		std::cout<<array_progs[i].burst_time<<"\t";	//output burst time
-	std::cout<<"\n";						//
+		printf("%d\t", array_progs[i].burst_time);	//output burst time
+	printf("\n");						//
 
-	std::cout<<"Waiting time:\t\t"; //output tempo di attesa
+	printf("Waiting time:\t\t"); //output tempo di attesa
 	int contatore_tempo=0; //tempo usato per esecuzione dei vari processi
 	int sommatoria_waitt=0; //sommatoria per la media
 	for(int i=0; i<num_progs; i++)
 	{
-		if(array_progs[i].arrival_time > contatore_tempo) //se il processo arriva dopo che tutti i processi piÃ¹ vecchi sono finiti
+		if(array_progs[i].arrival_time > contatore_tempo) //se il processo arriva dopo che tutti i processi piu vecchi sono finiti
 		{
-			std::cout<<contatore_tempo+(array_progs[i].arrival_time - contatore_tempo)<<"\t";
+			//std::cout<<contatore_tempo+(array_progs[i].arrival_time - contatore_tempo)<<"\t";
+			printf("%d\t", contatore_tempo+(array_progs[i].arrival_time - contatore_tempo));
 			sommatoria_waitt += contatore_tempo+(array_progs[i].arrival_time - contatore_tempo);
 			contatore_tempo += array_progs[i].burst_time + (array_progs[i].arrival_time - contatore_tempo);
 		}
 		else
 		{
-			std::cout<<contatore_tempo<<"\t";
+			printf("%d\t", contatore_tempo);
 			sommatoria_waitt += contatore_tempo;
 			contatore_tempo += array_progs[i].burst_time;
 		}
 	}
-	std::cout<<"\n";
+	printf("\n");
 
-	std::cout<<"Turn around time:\t"; //output turn around time
+	printf("Turn around time:\t"); //output turn around time
 	contatore_tempo=0; //tempo usato per esecuzione dei vari processi
 	int sommatoria_turnat=0;
 	for(int i=0; i<num_progs; i++)
 	{
-		if(array_progs[i].arrival_time > contatore_tempo) //se il processo arriva dopo che tutti i processi piÃ¹ vecchi sono finiti
+		if(array_progs[i].arrival_time > contatore_tempo) //se il processo arriva dopo che tutti i processi piu vecchi sono finiti
 		{
 			contatore_tempo += array_progs[i].burst_time + (array_progs[i].arrival_time - contatore_tempo);
-			std::cout<<contatore_tempo<<"\t";
+			printf("%d\t", contatore_tempo);
 			sommatoria_turnat += contatore_tempo;
 		}
 		else
 		{
 			contatore_tempo += array_progs[i].burst_time;
-			std::cout<<contatore_tempo<<"\t";
+			printf("%d\t", contatore_tempo);
 			sommatoria_turnat += contatore_tempo;
 		}
 	}
-	std::cout<<"\n";
+	printf("\n");
 
-	std::cout<<"Average wait time: "<<(((float) sommatoria_waitt)/((float) num_progs))<<"\n";		//output medie
-	std::cout<<"Average turn around time: "<<(((float) sommatoria_turnat)/((float) num_progs))<<"\n";	//
+	printf("Average wait time: %f\n", (((float) sommatoria_waitt)/((float) num_progs)));		//output medie
+	printf("Average turn around time: %f\n", (((float) sommatoria_turnat)/((float) num_progs)));	//
 }
 
 //appunti: (li ho lasciati perche' potrebbero rendere il programma di piu' facile lettura)
